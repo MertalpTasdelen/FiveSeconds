cf389b37553ddc52fd1e719102f280c29a26a57d
mertalptasdelen <alptasdelen@hotmail.com>
2018-07-15 01:21:32 +0300

    Some counter issue and adding different progress view

--- FiveSeconds/Controller/MainGameScreenViewController.swift
+++ FiveSeconds/Controller/MainGameScreenViewController.swift
@@ -18,10 +18,9 @@ class MainGameScreenViewController : UIViewController {
     
     @IBOutlet weak var lblTimeShow: UILabel!
     
-    @IBOutlet weak var progressView: UIProgressView!
-    
     @IBOutlet weak var btnUncoverQuestion: UIButton!
 
+    @IBOutlet weak var btnProgressView: RoundedButton!
     //MARK: IBActionss
     
     @IBAction func btnUncoverQuestion(_ sender: RoundedButton) {
@@ -31,14 +30,19 @@ class MainGameScreenViewController : UIViewController {
             btnState = btnState + 1
             sender.setTitle("Başlat", for: .normal)
             sender.setTitleColor(UIColor.flatWhite, for: .normal)
-            
+//            proggressViewStart()
+
         } else if btnState == 1 {
-            
+
             startTimer()
             sender.setTitle("Tamam", for: .normal)
             sender.setTitleColor(UIColor.flatWhite, for: .normal)
             btnState = btnState + 1
-
+            
+            if seconds == 1 || seconds == 1.00 || seconds == 1.0 {
+                timer.invalidate() //stoops the timer
+                lblTimeShow.text = "\(0)"
+            }
             
         } else if btnState == 2 {
             
@@ -53,8 +57,9 @@ class MainGameScreenViewController : UIViewController {
                 self.updateQuestionScreen(button: sender)
                 self.btnState = 0
                 self.resetTimer()
-                self.lblWhoWillPlay(player: self.turn)
+                self.lblWhoWillPlay(playerTurn: self.turn)
                 self.btnUncoverQuestion.setTitle("", for: .normal)
+                self.btnUncoverQuestion.backgroundColor = UIColor.randomFlat
             }))
             alert.addAction(UIAlertAction(title: "Yanlış", style: .default, handler: { (wrongAction) in
                 self.turn += 1
@@ -64,23 +69,14 @@ class MainGameScreenViewController : UIViewController {
                 self.updateQuestionScreen(button: sender)
                 self.btnState = 0
                 self.resetTimer()
-                self.lblWhoWillPlay(player: self.turn)
+                self.lblWhoWillPlay(playerTurn: self.turn)
                 self.btnUncoverQuestion.setTitle("", for: .normal)
                 self.btnUncoverQuestion.backgroundColor = UIColor.randomFlat
-
-
             }))
             self.present(alert, animated: true, completion: nil)
             
         }
     }
-    
-//    @IBAction func btnUncoverQuestionWithSwipe(_ sender: Any) {
-//        btnUncoverQuestion.setTitle(questionArray?[questionNumber].title ?? "No question here", for: .normal)
-//        self.updateQuestionScreen(button: sender as! UIButton)
-//        turn = turn + 1
-//
-//    }
     
     @IBAction func btnBack(_ sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
@@ -93,56 +89,102 @@ class MainGameScreenViewController : UIViewController {
     var playerArray: [Player] = [] //container for the player
     var questionArray: Results<Question>?
     var turn: Int = 0 //which player should answer the question
-    var seconds = Float(5.0)
+    let totalSeconds = Float(5.00)
+    var seconds = Float(5.00)
     var timer = Timer()
     var btnState = 0 //what is the state for the big button
     var isTimerRunning = false
     var questionNumber = 0 //specifies the qustion number
     
+    //MARK: For the progress view declerations
+    let shapeLayer = CAShapeLayer()
+    let ghostLayer = CAShapeLayer()
+    
+
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         //Initialization for the first player
         btnUncoverQuestion.backgroundColor = UIColor.randomFlat
-        lblWhoWillPlay(player: turn)
+        lblWhoWillPlay(playerTurn: turn)
         loadQuestions()
-//        for item in playerArray {
-//            print(item.name)
-//        }
+        ghostCircle()
+       
+    }
+    
+    func ghostCircle(){
+//        let center = view.center
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: 75.0, startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
+        
+        ghostLayer.path = circularPath.cgPath
+        
+        ghostLayer.lineWidth = 7
+        ghostLayer.strokeColor = UIColor.flatGrayDark.cgColor
+        ghostLayer.lineCap = kCALineCapRound
+        ghostLayer.fillColor = UIColor.clear.cgColor
+        ghostLayer.position = view.center
+        view.layer.addSublayer(ghostLayer)
+    }
+    
+    func proggressViewStart(){
+//        let center = view.center
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: 75.0, startAngle:0, endAngle: 2 * CGFloat.pi, clockwise: true)
+  
+//       Main layer for the progressview
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.lineWidth = 9
+        shapeLayer.strokeColor = UIColor.flatBlue.cgColor
+        shapeLayer.lineCap = kCALineCapRound
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.position = view.center
+        shapeLayer.transform = CATransform3DMakeRotation(-CGFloat.pi/2, 0, 0, 1)
+        shapeLayer.strokeEnd = 0
+        view.layer.addSublayer(shapeLayer)
+        
+        btnProgressView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(customCircularProgress)))
+
+    }
+    
+    @objc private func customCircularProgress(){
+        
+        let basicAnim = CABasicAnimation(keyPath: "strokeEnd")
+        
+        basicAnim.toValue = 1
+        basicAnim.duration = 5
+        
+        // Two lines code for the end of the anim.
+        basicAnim.fillMode = kCAFillModeForwards
+        basicAnim.isRemovedOnCompletion = false
+        
+        shapeLayer.add(basicAnim, forKey: "SoEasy")
         
 
     }
     
     //MARK: Start the timer
     func startTimer(){
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: (#selector(MainGameScreenViewController.updateTimer)), userInfo: nil, repeats: true)
-        progressView.setProgress(seconds, animated: true)
-        perform(#selector(updateTimer), with: nil, afterDelay: 0.0)
-        isTimerRunning = true
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: (#selector(updateTimer)), userInfo: nil, repeats: true)
+//        perform(#selector(updateTimer), with: nil)
+        //if timer hit 0 give an alert !!
     }
     
     @objc func updateTimer(){
-        seconds -= 1
-        lblTimeShow.text = "\(seconds)"
-        if seconds == 0 {
-            timer.invalidate() //stoops the timer
-            lblTimeShow.text = "\(0)"
-        }
+        seconds -= 0.01
+        lblTimeShow.text = String(format: "%.2f", seconds)
     }
     
     //MARK: Stop the timer
     func stopTimer(){
-        if isTimerRunning == true{
+        if timer.isValid == true{
             timer.invalidate()
-            isTimerRunning = false
         }
     }
     
     func resetTimer(){
-        seconds = 5.0
+        timer.invalidate()
+        seconds = 5
         lblTimeShow.text = "\(seconds)"
-        progressView.progress = seconds
     }
     
     func loadQuestions(){
@@ -155,12 +197,12 @@ class MainGameScreenViewController : UIViewController {
         questionNumber += 1
     }
     
-    func lblWhoWillPlay(player: Int) {
-        lblTurnSpecifier.text = "Sorunun sahibi \(playerArray[player].name)"
+    func lblWhoWillPlay(playerTurn: Int) {
+        lblTurnSpecifier.text = "Sorunun sahibi \(playerArray[playerTurn].name)"
         lblTurnSpecifier.textColor = UIColor.flatBlack
         lblTurnSpecifier.textAlignment = .center
     }
-    
+
     
     
     
